b1f8637892c856ea21949bd50aa275cc98926b9a
Code With Z <codewithz21@gmail.com>
2022-06-16 15:37:52 +0530

    Routing: Implemented Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <ul>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="/products">Products</a>
-            </li>
-            <li>
-                <a href="/posts/2022/02">Posts</a>
-            </li>
-            <li>
-                <a href="/admin">Admin</a>
-            </li>
-        </ul>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-success">
+            <Link className="navbar-brand" to="/">Smart Bank App</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+
+                    <Link className="nav-item nav-link" to="/">Home</Link>
+                    <Link className="nav-item nav-link" to="/products">Products</Link>
+                    <Link className="nav-item nav-link" to="/posts/2022/02">Posts</Link>
+                    <Link className="nav-item nav-link" to="/admin">Admin</Link>
+
+                </div>
+            </div>
+        </nav>
+
+
+
+
+
     )
 }
