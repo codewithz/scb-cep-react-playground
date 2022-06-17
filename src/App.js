531d1a30bfb8b7a8286e0d413c0a0b8f82935643
Code With Z <codewithz21@gmail.com>
2022-06-17 09:02:52 +0530

    Routing: Redirection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Products from './components/RoutingApp/Products';
 import Posts from './components/RoutingApp/Posts';
 import Dashboard from './components/RoutingApp/admin/Dashboard';
 import Home from './components/RoutingApp/Home';
 import ProductDetails from './components/RoutingApp/ProductDetails';
+import NotFound from './components/RoutingApp/NotFound';
 
 
 function App() {
@@ -21,7 +22,9 @@ function App() {
             <Route path="/products" render={(props) => <Products sortBy='newest' {...props} />} />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
